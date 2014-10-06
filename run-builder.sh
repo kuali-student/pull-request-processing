450f44813d345b29aea40048e4be8bf0d3af203c
Michael O'Cleirigh <michael.ocleirigh@utoronto.ca>
2014-10-06 11:37:11 -0400

    KSENROLL-14743 Add support for pushing db impex changes up to github
    Wire in the new PushGitReferenceMojo and use that for git repository

--- run-builder.sh
+++ run-builder.sh
@@ -8,7 +8,9 @@
 # PULL_REQUEST_NUMBER
 # PULL_REQUEST_COMMIT_ID
 #
-DEBUG_DB_OPTS=-Doracle.dba.password=X12p42Rs
+
+# for example set this to -Doracle.dba.password=<password>
+DEBUG_DB_OPTS=
 
 REPO_BASE=https://github.com/kuali-student
 
@@ -29,16 +31,19 @@ then
 	mkdir -p target
 
 	echo "Fetching pull request $PULL_REQUEST_NUMBER head at $PULL_REQUEST_COMMIT_ID"
-	mvn initialize -DfetchOpenPullRequests.target-pull-request-number=$PULL_REQUEST_NUMBER -DfetchOpenPullRequests.target-commit-id=$PULL_REQUEST_COMMIT_ID -DfetchOpenPullRequests.phase=initialize	
+	mvn process-resources -DfetchOpenPullRequests.target-pull-request-number=$PULL_REQUEST_NUMBER -DfetchOpenPullRequests.target-commit-id=$PULL_REQUEST_COMMIT_ID -DfetchOpenPullRequests.phase=process-resources -Dgit-workflow.fetchDepth=4	
 	
 	cd target/ks-repo
 
 	echo "Checkout $PR_BRANCH branch"
 	git checkout $PR_BRANCH	
 	
+	KS_REPO_PR_BRANCH_HEAD_ID=$(git log --format=%H -n 1 $PR_BRANCH)
+	echo "$PR_BRANCH at commit id: $KS_REPO_PR_BRANCH_HEAD_ID"
+	
 	# set the pom versions
 	## This will use the Fusion Tag Mojo
-	mvn initialize -Dfusion.tag.phase=initialize -Dfusion.tag.pull-request-number-property=PULL_REQUEST_NUMBER -N
+	mvn process-resources -Dfusion.tag.phase=process-resources -Dfusion.tag.pull-request-number-property=PULL_REQUEST_NUMBER -N
 	
 	# ideally this would work but if not then do a full build
 	# once development is building properly try switching this back.
@@ -62,27 +67,44 @@ then
 	if test 0 -eq $R
 	then
 		# branch exists remotely
+		echo "Branch $PR_BRANCH exists in the origin so checkout a local branch pointing at the existing one."
 		git checkout -b $PR_BRANCH origin/$PR_BRANCH
+		
 	else
 		# branch does not exist remotely
+		
+		echo "Branch $PR_BRANCH does not exist remotely so create it from master."
 		git checkout -b $PR_BRANCH
+		
+		echo "Setup the pom versions to $PR_BRANCH"
+		
+		# setup the pom versions.
+		mvn process-resources -Ppull-request -Dfusion.tag.phase=process-resources -Dfusion.tag.pull-request-number-property=PULL_REQUEST_NUMBER -Dfusion.tag.pull-request-number=$PULL_REQUEST_NUMBER -N -e
+	
 	fi
 	
-	# setup the pom versions.
-	mvn initialize -Ppull-request -Dfusion.tag.phase=initialize -Dfusion.tag.pull-request-number-property=PULL_REQUEST_NUMBER -Dfusion.tag.pull-request-number=$PULL_REQUEST_NUMBER -N -e
 	
-	# this only works on my local
-	mvn initialize -Plocal,source-db $DEBUG_DB_OPTS -N -e 
+	# run local manual impex process
+	
+	# this loads in all of the -sql module artifacts
+	mvn initialize -Plocal,source-db $DEBUG_DB_OPTS -N -e
+	
+	# this creates the .mpx and .xml files from the source database 
 	mvn generate-resources -Pdump,local $DEBUG_DB_OPTS -N -e
+	
+	# this moves the created .mpx and .xml files back under the src/main/resources directory.
 	mvn process-resources -Pimpexscm -N
 	
-	git add .
+	# we want to add all newly generated files
+	git add src/main/resources
 	
-	git commit -a -m'Commit Impex Changes for pull-request-$PULL_REQUEST_NUMBER'
+	git commit -a -m'Commit Impex Changes for pull-request-$PULL_REQUEST_NUMBER\n\nFor pull-request commit id: $KS_REPO_PR_BRANCH_HEAD_ID'
 	
-	echo "IMPEX_BRANCH=$PR_BRANCH" > pull-request-branch.dat
+	# move back up to the pull-request-builder directory
+	cd ../..
 	
-	# we want to push $PR_BRANCH to the upstream.
+	# update the ks-impex-repo $PR_BRANCH into github.
+	mvn process-resources -Dpush-db-changes.phase=process-resources  -Dpush-db-changes.pull-request-branch-name=$PR_BRANCH
 	
 
 else
